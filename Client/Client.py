21fc3049dd4a9adebf1f9ec45ca0e2efd189718f
c.pantoja <c.pantoja@uniandes.edu.co>
2021-03-10 00:10:57 -0500

    Server file sending protocol

--- Client/Client.py
+++ Client/Client.py
@@ -1,12 +1,11 @@
 from socket import *
 import hashlib
 
-serverName = '192.168.136.1'  # TBD IP de la maquina
+serverName = 'localhost'  # TBD IP de la maquina
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-
 
 data = clientSocket.recv(4096)
 
@@ -19,23 +18,23 @@ if data == ready:
     if data[0] == 'FILE_NAME':
         fileName = data[1]
         print('Nombre del archivo:', fileName)
-        
-        hasher = hashlib.new('sha256')        
+
+        hasher = hashlib.new('sha256')
         data = clientSocket.recv(4096)
         if data == 'FILE_INIT'.encode():
             bytesRecibidos = 0
-            with open(f'./ArchivosRecibidos/{fileName}','wb') as fileSend:
+            with open(f'./ArchivosRecibidos/{fileName}', 'wb') as fileSend:
                 i = 0
-                data = clientSocket.recv(4096) 
+                data = clientSocket.recv(4096)
                 bytesRecibidos += len(data)
                 while data and data != 'FILE_END'.encode():
                     hasher.update(data)
                     fileSend.write(data)
-                    data = clientSocket.recv(4096) 
+                    data = clientSocket.recv(4096)
                     i += 1
-                
+
             data = clientSocket.recv(4096).decode().split(':')
-            if data[0] ==  'HASH':
+            if data[0] == 'HASH':
                 hashData = data[1]
 
                 hashFile = hasher.hexdigest()
@@ -44,34 +43,17 @@ if data == ready:
                     print(f'Bytes recibidos: {bytesRecibidos}')
                     print('Descarga Exitosa')
                 else:
-                    print(f'Hash recibido del servidor: {hashData} \n Hash calculado del archivo: {hashFile}')    
-                    clientSocket.send('ERROR')    
-            else :
+                    print(f'Hash recibido del servidor: {hashData} \n Hash calculado del archivo: {hashFile}')
+                    clientSocket.send('ERROR')
+            else:
                 print(f'ERROR: se recibio {data[0]} y se esperaba "HASH"')
-        else :
+        else:
             recibido = data.decode()
             print('ERROR: se recibio {recibido} y se esperaba "FILE_INIT"')
     else:
         print('ERROR: se recibio {data[0]} y se esperaba "FILE_NAME"')
-else :
+else:
     recibido = data.decode()
     print('ERROR: se recibio {recibido} y se esperaba READY')
-                
 
-clientSocket.close()
-                
-
-
-                 
-            
-
-
-
-
-
-
-sentence = 'hola'
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server: ', modifiedSentence.decode())
 clientSocket.close()
--- Server/Server.py
+++ Server/Server.py
@@ -1,27 +1,88 @@
-# from SocketServer import ThreadingMixIn
 from socket import *
 from threading import Thread
+from threading import Lock
+import hashlib
+import time
 
 
 class ClientThread(Thread):
-    def __init__(self, connection, address):
+    def __init__(self, connection, address, ):
         Thread.__init__(self)
         self.connection = connection
         print("[+] New server socket thread started for " + address[0] + ":" + str(address[1]))
 
     def run(self):
-            sentence = self.connection.recv(1024).decode()
-            capitalizedSentence = sentence.upper()
-            self.connection.send(capitalizedSentence.encode())
-            self.connection.close()
+        conn = self.connection
+
+        # Client confirmation
+        conn.send(READY.encode())
+        cli = conn.recv(BUFFER_SIZE).decode()
+
+        if cli != READY:
+            raise
+
+        # Increase number of ready clients
+        numReadyLock.acquire()
+        numReady += 1
+        numReadyLock.release()
+
+        # Actively wait until all clients are ready
+        while numReady < numClients:
+            continue
+
+        hash_fn = hashlib.sha256()
+        file = open('./' + DIRECTORY + '/' + fileName)
+        total_sent = 0
+        chunks = 0
+
+        conn.send((FILE_NAME + SEP + fileName).encode())
+
+        # SEND FILE...
+        conn.send(FILE_INIT.encode())
+        f_read = file.read(BUFFER_SIZE)
+        t0 = time.time()  # Start timer
+        while len(f_read) > 0:
+            sent = conn.send(f_read)
+            total_sent += sent
+            chunks += 1
+            hash_fn.update(f_read)
+            f_read = file.read(BUFFER_SIZE)
+        conn.send(FILE_END.encode())
+
+        conn.send((HASH + SEP + hash_fn).encode())
+
+        # Client confirmation (OK | ERROR)
+        conn.recv(BUFFER_SIZE).decode()
+        conn.close()
 
 
-serverPort = 12000
-bufferSize = 1024
+# PROTOCOL COMMANDS IN CASE THEY ARE CHANGED
+READY = 'READY'
+FILE_NAME = 'FILE_NAME'
+SEP = ':'
+FILE_INIT = 'FILE_INIT'
+FILE_END = 'FILE_END'
+HASH = 'HASH'
+OK = 'OK'
+ERROR = 'ERROR'
+
+# Server options
+DIRECTORY = 'file'
+numClients = 5  # should be asked
+fileName = 'archivo'  # should be asked
+
+# Sync shared variables
+global numReady
+numReady = 0
+numReadyLock = Lock()
+
+# Socket Params
+SERVER_PORT = 12000
+BUFFER_SIZE = 4096
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('', serverPort))
+serverSocket.bind(('', SERVER_PORT))
 
 threads = []
 
@@ -29,9 +90,9 @@ while True:
     serverSocket.listen(25)
     print('The server is ready to receive')
 
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, address = serverSocket.accept()
 
-    newThread = ClientThread(connectionSocket, addr)
+    newThread = ClientThread(connectionSocket, address)
     newThread.start()
 
     threads.append(newThread)
