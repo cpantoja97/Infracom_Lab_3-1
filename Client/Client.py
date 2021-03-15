ac15c89390c53d2c8366e048c647bbc2a7a5fc1e
camilodi1999 <60122434+camilodi1999@users.noreply.github.com>
2021-03-15 11:45:33 -0500

    cliente encode

--- Client/Client.py
+++ Client/Client.py
@@ -2,7 +2,16 @@ from socket import *
 import hashlib
 from boltons import socketutils
 
-DELIMETER = ';'.encode()
+
+
+DELIMITER = ';'
+def receive(clientSocket):
+    return clientSocket.recv_until(DELIMITER.encode()).decode()
+
+def send(clientSocket, message):
+    encoded_message = (message + DELIMITER).encode()
+    clientSocket.send(encoded_message)
+
 serverName = 'localhost'  # TBD IP de la maquina
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -10,54 +19,53 @@ clientSocket.connect((serverName, serverPort))
 
 clientSocket = socketutils.BufferedSocket(clientSocket)
 
-data = clientSocket.recv_until(delimiter=DELIMETER)
+data = receive(clientSocket)
 
-ready = ('READY'+DELIMETER).encode()
+ready = 'READY'
 if data == ready:
-    clientSocket.send(ready)
+    send(clientSocket,ready)
 
-    data = clientSocket.recv_until(delimiter=DELIMETER)
-    data = data.decode().split(':')
+    data = receive(clientSocket)
+    data = data.split(':')
     if data[0] == 'FILE_NAME':
         fileName = data[1]
         print('Nombre del archivo:', fileName)
 
         hasher = hashlib.new('sha256')
-        data = clientSocket.recv_until(delimiter=DELIMETER)
-        if data == 'FILE_INIT'.encode():
+        data = receive(clientSocket)
+        if data == 'FILE_INIT':
             bytesRecibidos = 0
             with open(f'./ArchivosRecibidos/{fileName}', 'wb') as fileSend:
                 i = 0
-                data = clientSocket.recv_until(delimiter=DELIMETER)
-                while data and data != 'FILE_END'.encode():
+                data = receive(clientSocket)
+                while data and data != 'FILE_END':
                     bytesRecibidos += len(data)
                     hasher.update(data)
                     fileSend.write(data)
-                    data = clientSocket.recv_until(delimiter=DELIMETER)
+                    data = receive(clientSocket)
                     i += 1
 
-            data = clientSocket.recv_until(delimiter=DELIMETER).decode().split(':')
+            data =receive(clientSocket).split(':')
             if data[0] == 'HASH':
                 hashData = data[1]
 
                 hashFile = hasher.hexdigest()
                 if hashFile == hashData:
-                    clientSocket.send(('OK'+DELIMETER).encode())
+                    send(clientSocket,'OK')
                     print(f'Bytes recibidos: {bytesRecibidos}')
                     print('Descarga Exitosa')
                 else:
                     print(f'Hash recibido del servidor: {hashData} \n Hash calculado del archivo: {hashFile}')
-                    clientSocket.send(('ERROR'+DELIMETER).encode())
+                    send(clientSocket,'ERROR')
             else:
                 print(f'ERROR: se recibio {data[0]} y se esperaba "HASH"')
         else:
             recibido = data.decode()
-            print('ERROR: se recibio {recibido} y se esperaba "FILE_INIT"')
+            print(f'ERROR: se recibio {recibido} y se esperaba "FILE_INIT"')
             
     else:
-        print('ERROR: se recibio {data[0]} y se esperaba "FILE_NAME"')
+        print(f'ERROR: se recibio {data[0]} y se esperaba "FILE_NAME"')
 else:
-    recibido = data.decode()
-    print('ERROR: se recibio {recibido} y se esperaba READY')
+    print(f'ERROR: se recibio {data} y se esperaba READY')
 
 clientSocket.close()
