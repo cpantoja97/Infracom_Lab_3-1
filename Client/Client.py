12652706532d885619d944eda811d6908630ffd4
c.pantoja <c.pantoja@uniandes.edu.co>
2021-03-15 14:07:07 -0500

    style client

--- Client/Client.py
+++ Client/Client.py
@@ -2,15 +2,17 @@ from socket import *
 import hashlib
 from boltons import socketutils
 
-
-
 DELIMITER = ';'
+
+
 def receive(clientSocket):
     return clientSocket.recv_until(DELIMITER.encode()).decode()
+
 
 def send(clientSocket, message):
     encoded_message = (message + DELIMITER).encode()
     clientSocket.send(encoded_message)
+
 
 serverName = 'localhost'  # TBD IP de la maquina
 serverPort = 12000
@@ -23,7 +25,7 @@ data = receive(clientSocket)
 
 ready = 'READY'
 if data == ready:
-    send(clientSocket,ready)
+    send(clientSocket, ready)
 
     data = receive(clientSocket)
     data = data.split(':')
@@ -45,24 +47,24 @@ if data == ready:
                     data = receive(clientSocket)
                     i += 1
 
-            data =receive(clientSocket).split(':')
+            data = receive(clientSocket).split(':')
             if data[0] == 'HASH':
                 hashData = data[1]
 
                 hashFile = hasher.hexdigest()
                 if hashFile == hashData:
-                    send(clientSocket,'OK')
+                    send(clientSocket, 'OK')
                     print(f'Bytes recibidos: {bytesRecibidos}')
                     print('Descarga Exitosa')
                 else:
                     print(f'Hash recibido del servidor: {hashData} \n Hash calculado del archivo: {hashFile}')
-                    send(clientSocket,'ERROR')
+                    send(clientSocket, 'ERROR')
             else:
                 print(f'ERROR: se recibio {data[0]} y se esperaba "HASH"')
         else:
             recibido = data.decode()
             print(f'ERROR: se recibio {recibido} y se esperaba "FILE_INIT"')
-            
+
     else:
         print(f'ERROR: se recibio {data[0]} y se esperaba "FILE_NAME"')
 else:
