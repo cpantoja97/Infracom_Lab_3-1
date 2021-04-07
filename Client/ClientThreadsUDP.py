f9ac9647022f3e57de1aca44323c5416893ede8f
c.pantoja <c.pantoja@uniandes.edu.co>
2021-04-07 11:57:21 -0500

    Archivos para UDP basados en TCP

---
+++ Client/ClientThreadsUDP.py
@@ -0,0 +1,165 @@
+from socket import *
+from threading import Thread
+import hashlib
+import datetime
+from boltons import socketutils
+import pathlib
+import time
+
+def select():
+    clients = 0
+
+    # Inicio
+    print("Aplicacion del Cliente de Transferencia de Archivos")
+    print("Aquí podrá seleccionar un numero de clientes que van a solicitar un archivo en simultaneo")
+    print("Puede haber entre 1 y 25 conexiones. Si quiere finalizar ponga 0.")
+    
+    clients = int(input())
+    if (clients < 26 and clients != 0):
+        print("Se conectaran " + str(clients) + " clientes")    
+    
+    return clients
+
+class ClientThread(Thread):
+    def __init__(self,connection):
+        Thread.__init__(self)
+        self.socket = connection
+        self.ns_socket = socketutils.NetstringSocket(connection)
+
+    def receive(self):
+        return self.ns_socket.read_ns().decode()
+
+    def receive_file(self):
+        return self.ns_socket.read_ns()
+
+    def send(self, message):
+        encoded_message = message.encode()
+        self.ns_socket.write_ns(encoded_message)
+
+    def run(self):
+        conn = self.ns_socket
+        data = self.receive()
+
+        ready = 'READY'
+        if data == ready:
+            self.send(ready)
+            print('El cliente se conecto con el servidor')
+            
+            data = self.receive()
+            data = data.split(':')
+            idClient = data[0]
+            clients = data[1]    
+            
+            print(f'Se le asigno el id {idClient} al cliente')
+            data = self.receive()
+            data = data.split(':')
+            if data[0] == 'FILE_NAME':
+                fileName = data[1]
+                print('Nombre del archivo:', fileName)
+                
+                data = self.receive()
+                data = data.split(':')
+
+                if data[0] == 'FILE_SIZE':
+                    fileSize = data[1]
+                    print('Tamaño del archivo:', fileSize)
+                    hasher = hashlib.new('sha256')
+                    data = self.receive()
+                    if data == 'FILE_INIT':
+                        bytesRecibidos = 0
+                        pathlib.Path('./ArchivosRecibidos').mkdir(exist_ok=True)
+                        with open(f'./ArchivosRecibidos/Cliente{idClient}-Prueba-{clients}-{fileName}', 'wb') as fileSend:
+                            i = 0
+                            t0 = time.time()
+                            data = self.receive_file()
+                            while data and data != 'FILE_END'.encode():
+                                bytesRecibidos += len(data)
+                                hasher.update(data)
+                                fileSend.write(data)
+                                data = self.receive_file()
+                                i += 1
+                            t1 = time.time()
+                            self.send(RECIBIDO)
+                        data = self.receive().split(':')
+                        if data[0] == 'HASH':
+                            hashData = data[1]
+
+                            hashFile = hasher.hexdigest()
+                            status = ''
+                            if hashFile == hashData:
+                                self.send('OK')
+                                status = 'OK'
+                                print(f'Bytes recibidos: {bytesRecibidos}')
+                                print(f'Descarga Exitosa del cliente {idClient}')
+                            else:
+                                print(f'Hash recibido del servidor: {hashData} \n Hash calculado del archivo: {hashFile}')
+                                self.send('ERROR')
+                                status = 'ERROR'
+                        else:
+                            print(f'ERROR: se recibio {data[0]} y se esperaba "HASH"')
+
+                        self.send(str(bytesRecibidos) + ":" + str(i))
+                    else:
+                        recibido = data.decode()
+                        print(f'ERROR: se recibio {recibido} y se esperaba "FILE_INIT"')
+                else:
+                 print(f'ERROR: se recibio {data[0]} y se esperaba "FILE_SIZE"')   
+            else:
+                print(f'ERROR: se recibio {data[0]} y se esperaba "FILE_NAME"')
+        else:
+            print(f'ERROR: se recibio {data} y se esperaba READY')
+
+        self.socket.close()
+
+        log(idClient, clients, datetime.datetime.now(), status == OK, t1 - t0, fileName, fileSize, i, bytesRecibidos)
+    
+
+# Escritura del log
+def log(client_id, clients, now, exitosa, tiempoTotal, fileSelect, fileSize, enviados, bytesEnv):
+    # Crear file de log
+    formatname = "Cliente" + client_id + "-" + "Conexiones" + clients + "-" + now.strftime("%Y-%m-%d-%H-%M-%S") + "-log.txt"
+    pathlib.Path('./LogsClient').mkdir(exist_ok=True)
+    f = open("./LogsClient/" + formatname, "x")
+    # Nombre y tamano enviado
+    f.write("El nombre del archivo recibido es " + fileSelect + " y su tamaño es " + str(fileSize) + " B \n")
+    # Info transferencia
+    estado = "exitosa" if exitosa else "no exitosa"
+    f.write("La recepción del archivo fue " + estado + "\n")
+    f.write("El tiempo total de transferencia, en segundos, fue " + str(tiempoTotal) + "\n")
+    f.write("El numero de paquetes recibidos es " + str(enviados) + "\n")
+    f.write("El valor total en bytes recibidos es " + str(bytesEnv) + "\n")
+    f.close()
+    return formatname
+
+
+        
+
+serverName =  input('Dirección IP del servidor :') # '192.168.226.128'  # 'localhost' TBD IP de la maquina
+serverPort = 12000
+
+READY = 'READY'
+FILE_NAME = 'FILE_NAME'
+SEP = ':'
+FILE_INIT = 'FILE_INIT'
+FILE_END = 'FILE_END'
+HASH = 'HASH'
+OK = 'OK'
+ERROR = 'ERROR'
+RECIBIDO = 'RECEIVED'
+DIRECTORY = 'ArchivosRecibidos'
+
+
+threads = []
+
+numClients = select()
+
+for i in range(numClients):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    newThread = ClientThread(clientSocket)
+    newThread.start()
+
+    threads.append(newThread)
+
+for t in threads:
+    t.join()
---
+++ Server/ServerUDP.py
@@ -0,0 +1,171 @@
+from socket import *
+from threading import Thread
+from threading import Lock
+from boltons import socketutils
+import hashlib
+import time
+import datetime
+import pathlib
+from ServerView import select
+
+
+# Manage each Client connection
+class ClientThread(Thread):
+    def __init__(self, connection, address):
+        Thread.__init__(self)
+        self.address = address
+        self.socket = connection
+        self.ns_socket = socketutils.NetstringSocket(connection)  # Socket Wrapper to use Netstring wrapping technique
+        print("[+] New server socket thread started for " + address[0] + ":" + str(address[1]))
+
+    # Send a message in text format
+    def send_message(self, message):
+        self.ns_socket.write_ns(message.encode())
+
+    # Send file in binary
+    def send_file(self, chunk):
+        self.ns_socket.write_ns(chunk)
+
+    # Receive message in text format
+    def receive_message(self):
+        return self.ns_socket.read_ns().decode()
+
+    # Print a message in console preceding with client identification
+    def print_info(self, info):
+        print("[" + self.address[0] + ":" + str(self.address[1]) + "] " + info)
+
+    def run(self):
+        global numReady
+
+        # Client confirmation
+        self.send_message(READY)
+        cli = self.receive_message()
+        if cli != READY:
+            raise Exception(self.address + " Received " + cli + " instead of READY")
+        self.print_info("Client ready")
+
+        # Increase number of ready clients
+        numReadyLock.acquire()
+        numReady += 1
+        client_id = numReady
+        numReadyLock.release()
+
+        # Inform client of his number and total clients
+        self.send_message(str(client_id) + SEP + str(clients))
+
+        # Actively wait until all clients are ready
+        while numReady < clients:
+            continue
+        hash_fn = hashlib.sha256()
+        file = open('./' + DIRECTORY + '/' + fileSelect, "rb")
+        bytes_sent = 0
+        chunks_sent = 0
+
+        # Send file info
+        self.send_message(FILE_NAME + SEP + fileSelect)
+        self.send_message(FILE_SIZE + SEP + str(fileSize))
+
+        # SEND FILE...
+        self.send_message(FILE_INIT)
+        f_read = file.read(BUFFER_SIZE)
+        t0 = time.time()  # Start timer
+        while len(f_read) > 0:
+            self.send_file(f_read)
+            bytes_sent += len(f_read)
+            chunks_sent += 1
+            hash_fn.update(f_read)
+            f_read = file.read(BUFFER_SIZE)
+        self.send_message(FILE_END)
+
+        # Confirmación de recepcion
+        cli = self.receive_message()
+        if cli != RECEIVED:
+            raise Exception(self.address + " Received " + cli + " instead of RECEIVED")
+        t1 = time.time()  # End timer
+        self.print_info("File transferred")
+
+        # Send Hash
+        self.send_message(HASH + SEP + hash_fn.hexdigest())
+
+        # Client confirmation (OK | ERROR)
+        status = self.receive_message()
+        self.print_info("Client status " + status)
+
+        # Client bytes and packets received
+        cli = self.receive_message()
+        [bytes_received, chunks_received] = cli.split(":")
+        self.print_info("Client received " + str(bytes_received) + " bytes")
+        self.print_info("Client received " + str(chunks_received) + " packets")
+
+        # Close socket
+        self.socket.close()
+
+        # Write log
+        log(client_id, clients, self.address, datetime.datetime.now(), status == OK, t1 - t0, fileSelect, fileSize, chunks_sent, bytes_sent)
+
+
+# Escritura del log
+def log(client_id, clients, addr, now, exitosa, tiempoTotal, fileSelect, fileSize, enviados, bytesEnv):
+    # Crear file de log
+    formatname = "Cliente" + str(client_id) + "-" + "Conexiones" + str(clients) + "-" + now.strftime("%Y-%m-%d-%H-%M-%S") + "-log.txt"
+    pathlib.Path('./LogsServer').mkdir(exist_ok=True)
+    f = open("./LogsServer/" + formatname, "x")
+    # Nombre y tamano enviado
+    f.write("El nombre del archivo enviado es " + fileSelect + " y su tamaño es " + str(fileSize) + " B \n")
+    # Cliente
+    f.write("El cliente al que se realiza la transferencia es " + str(addr) + "\n")
+    # Info transferencia
+    estado = "exitosa" if exitosa else "no exitosa"
+    f.write("La entrega del archivo fue " + estado + "\n")
+    f.write("El tiempo total de transferencia, en segundos, fue " + str(tiempoTotal) + "\n")
+    f.write("El numero de paquetes enviados es " + str(enviados) + "\n")
+    f.write("El valor total en bytes enviado es " + str(bytesEnv) + "\n")
+    f.close()
+    return formatname
+
+
+# PROTOCOL COMMANDS IN CASE THEY ARE CHANGED
+READY = 'READY'
+FILE_NAME = 'FILE_NAME'
+FILE_SIZE = 'FILE_SIZE'
+SEP = ':'
+FILE_INIT = 'FILE_INIT'
+FILE_END = 'FILE_END'
+RECEIVED = 'RECEIVED'
+HASH = 'HASH'
+OK = 'OK'
+ERROR = 'ERROR'
+
+# Server options
+DIRECTORY = 'Files'
+
+# Sync shared variables
+numReady = 0
+numReadyLock = Lock()
+
+# Socket Params
+SERVER_PORT = 12000
+BUFFER_SIZE = 4096
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('', SERVER_PORT))
+
+threads = []
+
+# Corre el metodo del view para obtener los datos
+clients, fileSelect = select()
+fileSize = pathlib.Path('./' + DIRECTORY + '/' + fileSelect).stat().st_size
+
+while len(threads) < clients:
+    serverSocket.listen(clients)
+
+    connectionSocket, address = serverSocket.accept()
+
+    newThread = ClientThread(connectionSocket, address)
+    newThread.start()
+
+    threads.append(newThread)
+
+for t in threads:
+    t.join()
